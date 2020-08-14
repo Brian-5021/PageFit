28b47234d493703796e5113651318ace0aef03f0
Brian-5021 <brian05-21@hotmail.com>
2020-08-14 12:37:27 -0500

    configuracion auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { MisionvisionComponent } from './pages/misionvision/misionvision.component';
+import { RutinasComponent } from './pages/rutinas/rutinas.component';
+import { CrearutinaComponent } from './pages/crearutina/crearutina.component';
+import { AjustesComponent } from './pages/ajustes/ajustes.component';
+import { AuthGuard } from './services/auth.guard';
+
 
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
-  { path: '', pathMatch: 'full', redirectTo: 'path' }
+  {path: 'quienessomos', component: AboutusComponent},
+  {path: 'misionyvision', component: MisionvisionComponent},
+  {path: 'rutinas', component: RutinasComponent, canActivate: [ AuthGuard]},
+  {path: 'creaturutina', component: CrearutinaComponent},
+  {path: 'ajustes', component: AjustesComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
 ];
 
 @NgModule({
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd  } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +12,46 @@ export class NavbarComponent implements OnInit {
 
   events: string[] = [];
   opened: boolean;
+  // tslint:disable-next-line: no-inferrable-types
+  titleNav: string = '';
+  private myUrl: any;
 
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, public auth: AuthService ) {
+  }
 
   ngOnInit(): void {
+    this.router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        console.log(e.url);
+        const ruta = e.url;
 
+        switch (ruta) {
+          case '/inicio':
+            this.titleNav = 'Inicio de PageFit';
+            break;
+            case '/':
+            this.titleNav = 'Inicio de PageFit';
+            break;
+          case '/quienessomos':
+              this.titleNav = '¿Quiénes somos?';
+              break;
+          case '/misionyvision':
+                this.titleNav = 'Nuestra misión y visión';
+                break;
+          case '/rutinas':
+                  this.titleNav = 'Mira todas nuestras rutinas';
+                  break;
+          case '/creaturutina':
+                    this.titleNav = 'Aquí podrás personalizar tu rutina';
+                    break;
+          case '/ajustes':
+                      this.titleNav = 'Realiza los ajustes de la cuenta';
+                      break;
+          default:
+            break;
+        }
+       }
+     });
   }
 
 }
